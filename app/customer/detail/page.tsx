fe229fbcd8f77e1ce41e0e62dbfabe4d5fad3818
zhangzyg <zhangyouguzyg@163.com>
2025-06-18 21:53:29 +0800

    bug fix

--- app/customer/detail/page.tsx
+++ app/customer/detail/page.tsx
@@ -6,6 +6,6 @@ import { Suspense } from "react";
 export default function CustomerDetailPage() {
     return (<Suspense fallback={<div>加载中...</div>}>
         <CustomerDetail />
-    </Suspense>
+    </Suspense> 
     );
 }
